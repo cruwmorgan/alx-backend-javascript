8b9af276386a2cbbd3de8129328ffd2b1d44c304
cruwmorgan <morganokeke@gmail.com>
2023-07-27 05:42:11 -0700

    resolve all promises and log body firstName lastName to the console.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,14 @@
-/*import uploadPhoto and createUser from utils.js*/
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-	return Promise.all([uploadPhoto(), createUser()])
-	.then((result) => { 
-		console.log(
-                  `${result[0].body} ${result[1].firstName} ${result[1].lastName}`,
-		);
-	})
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => { 
+      console.log(
+        `${result[0].body} ${result[1].firstName} ${result[1].lastName}`,
+      );
+    })
 
-    	.catch((error) => {
-		console.log('Signup system offline');
-	});
+  .catch((error) => {
+    console.log('Signup system offline');
+  });
 }
