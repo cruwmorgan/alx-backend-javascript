7459abfd2ee4e1c2cee60e67363db11662fec1e4
ctuwmorgan <morganokeke@gmail.com>
2023-09-22 05:27:48 +0000

    Combining descriptions

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('should round two numbers and return their sum', function () {
