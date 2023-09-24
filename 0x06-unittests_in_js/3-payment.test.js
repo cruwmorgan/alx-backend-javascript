5b36037032c9745da1d27fd0727efb99708e8ad9
ctuwmorgan <morganokeke@gmail.com>
2023-09-24 10:24:54 +0000

    SPies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,18 +1,20 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
+const { expect } = require('chai');
+
 const sendPaymentRequestToApi = require('./3-payment');
-const { assert } = require('chai');
+const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', () => {
-  afterEach(() => {
-    sinon.restore(); // Restore the spy after each test.
+describe('Spies', function () {
+  it('has the same math', () => {
+    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+    const spyConsole = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    spyUtils.restore();
+    spyConsole.restore();
   });
-
-  it('should call Utils.calculateNumber with the correct arguments', () => {
-    const calculateNumberSpy = sinon.spy(Utils, "calculateNumber");
-    // Call the function under test
-    Utils.calculateNumber('SUM', 100, 20);
-    let spyCall = Utils.calculateNumber.getCall(0);
-    assert.equal(spyCall.returnValue, sendPaymentRequestToApi(100, 20));
-  })
-})
+});
